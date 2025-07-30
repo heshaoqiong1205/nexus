09e9affa8f91d20328ccdf8b19e1316befc3e62a
何少琼 <heshaoqiong@tuya.com>
2025-07-31 00:06:48 +0800

    config

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -8,24 +8,7 @@ import (
 )
 
 type App struct {
-	JwtSecret string
-	PageSize  int
-	PrefixUrl string
-
-	RuntimeRootPath string
-
-	ImageSavePath  string
-	ImageMaxSize   int
-	ImageAllowExts []string
-
-	ExportSavePath string
-	QrCodeSavePath string
-	FontSavePath   string
-
-	LogSavePath string
-	LogSaveName string
-	LogFileExt  string
-	TimeFormat  string
+	Name string `ini:"name"`
 }
 
 var AppSetting = &App{}
@@ -75,7 +58,6 @@ func Setup() {
 	mapTo("database", DatabaseSetting)
 	mapTo("redis", RedisSetting)
 
-	AppSetting.ImageMaxSize = AppSetting.ImageMaxSize * 1024 * 1024
 	ServerSetting.ReadTimeout = ServerSetting.ReadTimeout * time.Second
 	ServerSetting.WriteTimeout = ServerSetting.WriteTimeout * time.Second
 	RedisSetting.IdleTimeout = RedisSetting.IdleTimeout * time.Second
